771dd331ca12a1d0f4278bb6d37650c1aa6e8f04
disrupted <hi@salomonpopp.me>
2021-01-16 13:51:09 +0100

    nvim/efm: update isort settings

--- .config/nvim/lua/efm/isort.lua
+++ .config/nvim/lua/efm/isort.lua
@@ -1,5 +1,5 @@
--- return {formatCommand = "isort --stdout --profile black -", formatStdin = true}
 return {
-    formatCommand = "isort --quiet --stdout --sp backend/.isort.cfg --lines-between-types=1 -",
+    -- formatCommand = "isort --quiet --stdout --sp backend/.isort.cfg --lines-between-types=1 -",
+    formatCommand = "isort --quiet --stdout --profile black -",
     formatStdin = true
 }
