177455e6f73ad9aebdd49c545e7ad777e3351942
disrupted <hi@salomonpopp.me>
2021-02-10 20:53:19 +0100

    Switch to async formatting

--- .config/nvim/lua/conf/lsp.lua
+++ .config/nvim/lua/conf/lsp.lua
@@ -107,8 +107,10 @@ function M.config()
         if client.resolved_capabilities.document_formatting then
             vim.api.nvim_command [[augroup Format]]
             vim.api.nvim_command [[autocmd! * <buffer>]]
+            -- vim.api
+            --     .nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)]]
             vim.api
-                .nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)]]
+                .nvim_command [[autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()]]
             vim.api.nvim_command [[augroup END]]
         end
 
