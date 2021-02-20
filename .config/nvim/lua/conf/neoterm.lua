e8d07e5b6288627cbc3f2891b7b762a53badaf95
disrupted <hi@salomonpopp.me>
2021-02-20 01:04:27 +0100

    Add neoterm config

---
+++ .config/nvim/lua/conf/neoterm.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+function M.config()
+    vim.g.neoterm_default_mod = 'vertical'
+    vim.g.neoterm_size = 80
+    vim.g.neoterm_autoinsert = 1
+    vim.g.neoterm_autoscroll = 1
+    vim.g.neoterm_term_per_tab = 1
+    -- Key bindings
+    local opts = {noremap = true, silent = true}
+    vim.api.nvim_set_keymap("n", "<C-q>", ":Ttoggle<CR>", opts)
+    vim.api.nvim_set_keymap("i", "<C-q>", "<Esc>:Ttoggle<CR>", opts)
+    vim.api.nvim_set_keymap("t", "<C-q>", "<C-\\><C-N>:Ttoggle<CR>", opts)
+end
+
+return M
