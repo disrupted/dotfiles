4d95f268d6853db4b373b20ee0c686d13261cbb5
disrupted <hi@salomonpopp.me>
2021-03-20 19:29:38 +0100

    Fix nvim-tree config

--- .config/nvim/lua/conf/nvim_tree.lua
+++ .config/nvim/lua/conf/nvim_tree.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.setup()
+    vim.cmd [[packadd nvim-tree.lua]]
     vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
     vim.g.nvim_tree_auto_open = 0 -- 0 by default, opens the tree when typing `nvim $DIR` or `nvim`
     vim.g.nvim_tree_auto_close = 1 -- 0 by default, closes the tree when it's the last window
@@ -14,27 +15,31 @@ function M.setup()
     vim.g.nvim_tree_allow_resize = 1 -- 0 by default, will not resize the tree when opening a file
     vim.g.nvim_tree_disable_keybindings = 0
     --  modify some of the key mappings
+    local tree_cb = require'nvim-tree.config'.nvim_tree_callback
     vim.g.nvim_tree_bindings = {
-        edit = {'<CR>', 'o', 'l'},
-        edit_vsplit = {'<C-v>', '|'},
-        edit_split = {'<C-x>', '-'},
-        edit_tab = '<C-t>',
-        close_node = {'<S-CR>', 'h'},
-        toggle_ignored = '!',
-        toggle_dotfiles = '.',
-        refresh = 'R',
-        preview = '<Tab>',
-        cd = '<C-]>',
-        create = 'a',
-        remove = '<BS>',
-        rename = 'r',
-        cut = 'dd',
-        copy = 'yy',
-        paste = 'p',
-        prev_git_item = '[c',
-        next_git_item = ']c',
-        dir_up = '-',
-        close = 'q'
+        ["<CR>"] = tree_cb("edit"),
+        ["o"] = tree_cb("edit"),
+        ["l"] = tree_cb("edit"),
+        ["<C-v>"] = tree_cb("vsplit"),
+        ["<C-x>"] = tree_cb("split"),
+        ["<C-t>"] = tree_cb("tabnew"),
+        ["<S-CR>"] = tree_cb("close_node"),
+        ["h"] = tree_cb("close_node"),
+        ["!"] = tree_cb("toggle_ignored"),
+        ["."] = tree_cb("toggle_dotfiles"),
+        ["R"] = tree_cb("refresh"),
+        ["a"] = tree_cb("create"),
+        ["<BS>"] = tree_cb("remove"),
+        ["r"] = tree_cb("rename"),
+        ["<C-r>"] = tree_cb("full_rename"),
+        ["dd"] = tree_cb("cut"),
+        ["yy"] = tree_cb("copy"),
+        ["p"] = tree_cb("paste"),
+        ["[c"] = tree_cb("prev_git_item"),
+        ["]c"] = tree_cb("next_git_item"),
+        ["-"] = tree_cb("dir_up"),
+        ["<C-]>"] = tree_cb("cd"),
+        ["q"] = tree_cb("close")
     }
 
     vim.g.nvim_tree_icons = {
@@ -55,7 +60,8 @@ function M.setup()
       hi NvimTreeFolderIcon guifg=#5c6370
     ]])
 
-    vim.api.nvim_set_keymap("n", "<C-e>", "<cmd>NvimTreeToggle<CR>",
+    vim.api.nvim_set_keymap("n", "<C-e>",
+                            "<cmd>lua require 'nvim-tree'.toggle()<CR>",
                             {noremap = true, silent = true})
 end
 
