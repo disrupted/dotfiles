477e87c8b4c5ed7ae3374fd3b97d0916e06ca740
disrupted <hi@salomonpopp.me>
2021-03-22 23:03:12 +0100

    Update indentline config

--- .config/nvim/lua/conf/indentline.lua
+++ .config/nvim/lua/conf/indentline.lua
@@ -4,6 +4,8 @@ function M.config()
     vim.g.indent_blankline_char = '│'
     vim.g.indent_blankline_show_first_indent_level = false
     vim.g.indent_blankline_char_highlight = 'Whitespace'
+    vim.g.indent_blankline_filetype_exclude = {'help', 'markdown'}
+    vim.g.indent_blankline_buftype_exclude = {'terminal', 'nofile', 'packer'}
 end
 
 return M
