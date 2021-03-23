4ab2391ef953600eaef9d8ce97392988d5e1a24e
disrupted <hi@salomonpopp.me>
2021-03-23 20:21:49 +0100

    Disable indentline for gitcommit

--- .config/nvim/lua/conf/indentline.lua
+++ .config/nvim/lua/conf/indentline.lua
@@ -4,7 +4,7 @@ function M.config()
     vim.g.indent_blankline_char = '│'
     vim.g.indent_blankline_show_first_indent_level = false
     vim.g.indent_blankline_char_highlight = 'Whitespace'
-    vim.g.indent_blankline_filetype_exclude = {'help', 'markdown'}
+    vim.g.indent_blankline_filetype_exclude = {'help', 'markdown', 'gitcommit'}
     vim.g.indent_blankline_buftype_exclude = {'terminal', 'nofile', 'packer'}
 end
 
