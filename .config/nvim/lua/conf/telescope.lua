1525392f7588624c8e599fa8407a8b179e8acf3f
disrupted <hi@salomonpopp.me>
2021-04-12 23:10:50 +0200

    Display buffers only from current cwd in Telescope switcher
    Useful if using tabs for different projects/workspaces:
    https://github.com/nvim-telescope/telescope.nvim/pull/739

--- .config/nvim/lua/conf/telescope.lua
+++ .config/nvim/lua/conf/telescope.lua
@@ -30,6 +30,7 @@ function M.setup()
             layout_strategy = 'horizontal',
             layout_config = {preview_width = 0.65},
             show_all_buffers = true,
+            only_cwd = true,
             color_devicons = true
         })
     end
