b688c09ab1527f29b3982c5edd247bc2eeddb1c0
disrupted <hi@salomonpopp.me>
2021-04-21 00:36:27 +0200

    Fix indent-blankline artifacts from cursorline

--- .config/nvim/lua/conf/indentline.lua
+++ .config/nvim/lua/conf/indentline.lua
@@ -1,12 +1,19 @@
 local M = {}
 
 function M.config()
+    vim.wo.colorcolumn = "99999" --  workaround for cursorline causing artifacts
     vim.g.indent_blankline_char = '▏'
     vim.g.indent_blankline_show_first_indent_level = false
-    vim.g.indent_blankline_char_highlight = 'Whitespace'
     vim.g.indent_blankline_filetype_exclude =
         {'help', 'markdown', 'gitcommit', 'packer'}
     vim.g.indent_blankline_buftype_exclude = {'terminal', 'nofile'}
+    vim.g.indent_blankline_use_treesitter = true
+    -- vim.g.indent_blankline_show_current_context = true
+    -- vim.g.indent_blankline_context_patterns =
+    --     {
+    --         'class', 'function', 'method', '^if', '^while', '^for', '^object',
+    --         '^table', 'block', 'arguments'
+    --     }
 end
 
 return M
