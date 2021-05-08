cf8934854e73de17e747bb0d750d7def237edea8
disrupted <hi@salomonpopp.me>
2021-05-08 23:22:02 +0200

    Configure <> pairs

--- .config/nvim/lua/conf/pears.lua
+++ .config/nvim/lua/conf/pears.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.config()
     require'pears'.setup(function(conf)
+        conf.pair('<', '>')
         conf.preset 'tag_matching'
         conf.on_enter(function(pears_handle)
             if vim.fn.pumvisible() == 1 and vim.fn.complete_info().selected ~=
