370041e42004fe073a7aefa04b6257b5c47cfba9
disrupted <hi@salomonpopp.me>
2021-05-11 11:55:07 +0200

    Extend surround pairs with <>

--- .config/nvim/lua/conf/surround.lua
+++ .config/nvim/lua/conf/surround.lua
@@ -2,6 +2,11 @@ local M = {}
 
 function M.config()
     vim.g.surround_mappings_style = 'surround'
+    vim.g.surround_pairs = {
+        nestable = {{'(', ')'}, {'[', ']'}, {'{', '}'}, {'<', '>'}},
+        linear = {{'\'', '\''}, {'"', '"'}}
+    }
+    vim.g.surround_brackets = {'(', '{', '[', '<'}
     require'surround'.setup {}
 end
 
