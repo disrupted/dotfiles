727a2941564b7205b68dba809b0222c96c539a82
disrupted <hi@salomonpopp.me>
2021-05-20 00:28:07 +0200

    Fix LSP references always open instead of toggle

--- .config/nvim/lua/conf/lsp.lua
+++ .config/nvim/lua/conf/lsp.lua
@@ -172,7 +172,7 @@ function M.config()
             '<cmd>lua vim.lsp.buf.rename()<CR>',
             opts
         )
-        buf_set_keymap('n', 'gr', '<cmd>TroubleToggle lsp_references<CR>', opts)
+        buf_set_keymap('n', 'gr', '<cmd>Trouble lsp_references<CR>', opts)
         buf_set_keymap('n', 'gR', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
         buf_set_keymap(
             'n',
