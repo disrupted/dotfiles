cb3b6dc1130ad8a350581f9b6a66c7904c9e9844
disrupted <hi@salomonpopp.me>
2021-08-19 11:38:16 +0200

    Improve LSP formatting of Dockerfiles

--- .config/nvim/lua/conf/lsp.lua
+++ .config/nvim/lua/conf/lsp.lua
@@ -344,6 +344,15 @@ function M.config()
         on_attach = on_attach,
         capabilities = capabilities,
         flags = { debounce_text_changes = 150 },
+        settings = {
+            docker = {
+                languageserver = {
+                    formatter = {
+                        ignoreMultilineInstructions = true,
+                    },
+                },
+            },
+        },
     }
 
     -- YAML
