b621197fbe2a3cc89fcf0974516153c8eded2681
disrupted <hi@salomonpopp.me>
2021-09-06 18:27:21 +0200

    Set shfmt indentation to 4 spaces

--- .config/nvim/lua/conf/null-ls.lua
+++ .config/nvim/lua/conf/null-ls.lua
@@ -46,12 +46,14 @@ function M.config()
                 vim.fn.expand '~/.config/stylua.toml',
             },
         },
-        blackd,
         isortd,
+        blackd,
         null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.shfmt.with {
+            extra_args = { '-i', '4', '-ci' },
+        },
         null_ls.builtins.diagnostics.shellcheck,
     }
     null_ls.config {
