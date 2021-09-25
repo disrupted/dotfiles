dc979aa2d35f618beccd6e543e06971de8d28bdd
disrupted <hi@salomonpopp.me>
2021-09-25 21:49:12 +0200

    Update gitsigns config
    add new configuration options

--- .config/nvim/lua/conf/gitsigns.lua
+++ .config/nvim/lua/conf/gitsigns.lua
@@ -49,8 +49,10 @@ function M.config()
             [9] = '₉',
             ['+'] = '₊',
         },
+        signcolumn = true,
         numhl = false,
         linehl = false,
+        word_diff = false,
         keymaps = {
             -- Default keymap options
             noremap = true,
@@ -66,17 +68,43 @@ function M.config()
             },
 
             ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+            ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
             ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
             ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+            ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
             ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
             ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
             ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+            ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
+            ['n <leader>hU'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
+
+            -- Text objects
+            ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+            ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
         },
         watch_gitdir = { interval = 1000 },
+        attach_to_untracked = true,
         current_line_blame = false,
+        current_line_blame_opts = {
+            virt_text = true,
+            virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+            delay = 1000,
+        },
+        current_line_blame_formatter_opts = {
+            relative_time = false,
+        },
         sign_priority = 6,
         update_debounce = 100,
         status_formatter = nil, -- Use default
+        max_file_length = 40000,
+        preview_config = {
+            -- Options passed to nvim_open_win
+            border = 'single',
+            style = 'minimal',
+            relative = 'cursor',
+            row = 0,
+            col = 1,
+        },
         diff_opts = { internal = true },
         yadm = { enable = true },
     }
