3f3730ac45d6c42f02c2412f054d110f6f3097cb
disrupted <hi@salomonpopp.me>
2021-10-29 22:26:00 +0200

    chore(tmux): add mapping for tmux pane zoom

--- .config/nvim/lua/conf/comment.lua
+++ .config/nvim/lua/conf/comment.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.config()
     require('Comment').setup {
         ignore = '^$', -- ignore empty lines
-        pre_hook = function(ctx)
+        pre_hook = function(_)
             if vim.bo.filetype == 'typescriptreact' then
                 return require('ts_context_commentstring.internal').calculate_commentstring()
             end
