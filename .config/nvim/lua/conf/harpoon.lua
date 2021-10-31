8cce1c67869a66b152dadb7d944e98cfe8afbb15
disrupted <hi@salomonpopp.me>
2021-10-31 21:52:24 +0100

    feat(harpoon): configure mappings for jumping between files

--- .config/nvim/lua/conf/harpoon.lua
+++ .config/nvim/lua/conf/harpoon.lua
@@ -2,8 +2,12 @@ local M = {}
 
 function M.setup()
     local map = require('utils').map
-    map('n', '"', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>')
+    map('n', ';;', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>')
     map('n', 'M', '<cmd>lua require("harpoon.mark").toggle_file()<CR>')
+    map('n', ';a', '<cmd>lua require("harpoon.ui").nav_file(1)<CR>')
+    map('n', ';s', '<cmd>lua require("harpoon.ui").nav_file(2)<CR>')
+    map('n', ';d', '<cmd>lua require("harpoon.ui").nav_file(3)<CR>')
+    map('n', ';f', '<cmd>lua require("harpoon.ui").nav_file(4)<CR>')
 
     -- Use common mappings to close popup
     vim.cmd [[ 
