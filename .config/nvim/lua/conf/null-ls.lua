b42f3ea095eeb63717e47fac0745ce6d89208e06
disrupted <hi@salomonpopp.me>
2021-11-02 23:40:09 +0100

    feat(null-ls): setup dprint as custom formatter

--- .config/nvim/lua/conf/null-ls.lua
+++ .config/nvim/lua/conf/null-ls.lua
@@ -38,6 +38,32 @@ function M.config()
         },
     }
 
+    local dprint = {
+        name = 'dprint',
+        method = null_ls.methods.FORMATTING,
+        filetypes = {
+            'json',
+            'markdown',
+            'javascript',
+            'javascriptreact',
+            'typescript',
+            'typescriptreact',
+            'toml',
+            'dockerfile',
+        },
+        generator = h.formatter_factory {
+            command = 'dprint',
+            args = {
+                'fmt',
+                '--config',
+                vim.fn.expand '~/.config/dprint.json',
+                '--stdin',
+                '$FILEEXT',
+            },
+            to_stdin = true,
+        },
+    }
+
     -- sources
     local sources = {
         null_ls.builtins.formatting.stylua.with {
@@ -47,13 +73,26 @@ function M.config()
         },
         isortd,
         blackd,
-        null_ls.builtins.formatting.prettierd,
+        dprint,
+        null_ls.builtins.formatting.prettierd.with {
+            filetypes = {
+                'vue',
+                'svelte',
+                'css',
+                'scss',
+                'less',
+                'html',
+                'yaml',
+                'graphql',
+            },
+        },
         null_ls.builtins.formatting.shfmt.with {
             extra_args = { '-i', '4', '-ci' },
         },
-        null_ls.builtins.diagnostics.shellcheck,
         -- null_ls.builtins.formatting.trim_whitespace,
         -- null_ls.builtins.formatting.trim_newlines,
+        null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.code_actions.refactoring,
     }
     null_ls.config {
         sources = sources,
