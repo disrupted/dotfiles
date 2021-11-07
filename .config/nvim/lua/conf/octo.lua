dd50c0cc24ffc72ca50f94f66cfe9928cbf5b9ee
disrupted <hi@salomonpopp.me>
2021-11-07 14:00:16 +0100

    feat(octo): open current PR or fallback to list all

--- .config/nvim/lua/conf/octo.lua
+++ .config/nvim/lua/conf/octo.lua
@@ -2,8 +2,19 @@ local M = {}
 
 function M.setup()
     local map = require('utils').map
-    map('n', '<leader>op', '<cmd>Octo pr list<CR>')
+    map('n', '<leader>op', '<cmd>lua __octo_open_pr()<CR>')
     map('n', '<leader>oi', '<cmd>Octo issue list<CR>')
+
+    function _G.__octo_open_pr()
+        local url = vim.fn.system 'gh pr view --json url --jq .url 2>/dev/null'
+        if url then
+            vim.notify(url)
+            local cmd = string.format('Octo %s', url)
+            vim.cmd(cmd)
+        else
+            vim.cmd 'Octo pr list'
+        end
+    end
 end
 
 function M.config()
