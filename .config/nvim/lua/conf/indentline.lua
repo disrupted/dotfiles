a06169d23e66f511974623d40ae422863b4ce79a
disrupted <hi@salomonpopp.me>
2021-11-07 14:13:27 +0100

    chore(vim): migrate indentline config to setup method

--- .config/nvim/lua/conf/indentline.lua
+++ .config/nvim/lua/conf/indentline.lua
@@ -2,29 +2,35 @@ local M = {}
 
 function M.config()
     vim.opt.colorcolumn = '99999' --  workaround for cursorline causing artifacts
-    vim.g.indent_blankline_char = '▏'
-    vim.g.indent_blankline_show_first_indent_level = false
-    vim.g.indent_blankline_filetype_exclude = {
-        'help',
-        'markdown',
-        'gitcommit',
-        'packer',
+
+    require('indent_blankline').setup {
+        char = '▏',
+        context_char = '▏',
+        show_first_indent_level = false,
+        filetype_exclude = {
+            'help',
+            'markdown',
+            'gitcommit',
+            'packer',
+        },
+        buftype_exclude = { 'terminal', 'nofile' },
+        use_treesitter = true,
+        -- show_current_context = true,
+        -- context_patterns = {
+        --     'class',
+        --     'function',
+        --     'method',
+        --     '^if',
+        --     '^while',
+        --     '^for',
+        --     '^object',
+        --     '^table',
+        --     'block',
+        --     'arguments',
+        -- },
     }
-    vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
-    vim.g.indent_blankline_use_treesitter = true
-    -- vim.g.indent_blankline_show_current_context = true
-    -- vim.g.indent_blankline_context_patterns = {
-    --     'class',
-    --     'function',
-    --     'method',
-    --     '^if',
-    --     '^while',
-    --     '^for',
-    --     '^object',
-    --     '^table',
-    --     'block',
-    --     'arguments',
-    -- }
+
+    -- vim.cmd 'highlight! link IndentBlanklineContextChar Comment'
 end
 
 return M
