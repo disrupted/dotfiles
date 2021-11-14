a239046ab54f6c4b30bc2f4403f2c1c8857cc93b
disrupted <hi@salomonpopp.me>
2021-11-14 20:53:15 +0100

    chore(treesitter): switch to builtin parser for http filetype

--- .config/nvim/lua/conf/treesitter.lua
+++ .config/nvim/lua/conf/treesitter.lua
@@ -4,18 +4,6 @@ function M.config()
     vim.cmd [[packadd nvim-treesitter-refactor]]
     vim.cmd [[packadd nvim-treesitter-textobjects]]
 
-    local parser_configs =
-        require('nvim-treesitter.parsers').get_parser_configs()
-
-    -- http parser
-    parser_configs.http = {
-        install_info = {
-            url = 'https://github.com/NTBBloodbath/tree-sitter-http',
-            files = { 'src/parser.c' },
-            branch = 'main',
-        },
-    }
-
     require('nvim-treesitter.configs').setup {
         ensure_installed = 'maintained',
         highlight = { enable = true },
