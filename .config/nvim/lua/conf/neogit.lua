c478e86925aa4b45f559878358846a99e91dc30b
disrupted <hi@salomonpopp.me>
2021-11-25 11:43:01 +0100

    feat(neogit): assign mapping to git commit action

--- .config/nvim/lua/conf/neogit.lua
+++ .config/nvim/lua/conf/neogit.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.setup()
     local map = require('utils').map
     map('n', '<space>g', '<cmd>lua require("neogit").open()<CR>')
+    map('n', '<space>c', '<cmd>lua require("neogit").open { "commit" }<CR>')
 end
 
 function M.config()
