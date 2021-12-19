751638a21ce3c22f4848bade40756e9426e4bf9e
disrupted <hi@salomonpopp.me>
2021-12-19 22:45:29 +0100

    feat(nvim): lightspeed ignore-case

--- .config/nvim/lua/conf/lightspeed.lua
+++ .config/nvim/lua/conf/lightspeed.lua
@@ -30,6 +30,7 @@ end
 
 function M.config()
     require('lightspeed').setup {
+        ignore_case = true,
         exit_after_idle_msecs = { labeled = 4000, unlabeled = 3000 },
         x_mode_prefix_key = '<c-x>', -- TODO
     }
