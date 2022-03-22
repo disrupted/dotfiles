ac36aa38f90e6d34986e818422aca00b8f7fcb82
disrupted <hi@salomonpopp.me>
2022-03-22 22:43:30 +0100

    feat(nvim): filter Pyright LSP diagnostics

--- .config/nvim/lua/conf/lsp.lua
+++ .config/nvim/lua/conf/lsp.lua
@@ -28,6 +28,49 @@ function M.setup()
         severity_sort = true,
     }
 
+    local function filter(arr, func)
+        -- general purpose filter in place
+        -- https://stackoverflow.com/questions/49709998/how-to-filter-a-lua-array-inplace
+        local new_index = 1
+        local size_orig = #arr
+        for old_index, v in ipairs(arr) do
+            if func(v, old_index) then
+                arr[new_index] = v
+                new_index = new_index + 1
+            end
+        end
+        for i = new_index, size_orig do
+            arr[i] = nil
+        end
+    end
+
+    local function filter_diagnostics(diagnostic)
+        -- only apply filter to Pyright & Pylance
+        if diagnostic.source ~= 'Py' then
+            return true
+        end
+
+        -- Allow kwargs to be unused
+        if diagnostic.message == '"kwargs" is not accessed' then
+            return false
+        end
+
+        -- prefix variables with an underscore to ignore
+        if string.match(diagnostic.message, '"_.+" is not accessed') then
+            return false
+        end
+
+        return true
+    end
+
+    vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+        function(_, params, ctx, config)
+            filter(params.diagnostics, filter_diagnostics)
+            vim.lsp.diagnostic.on_publish_diagnostics(_, params, ctx, config)
+        end,
+        {}
+    )
+
     -- Handle formatting in a smarter way
     -- If the buffer has been edited before formatting has completed, do not try to
     -- apply the changes, by Lukas Reineke
