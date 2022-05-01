03e4015307695c0b9fa4a6aabfb1345473331b35
disrupted <hi@salomonpopp.me>
2022-05-01 15:08:09 +0200

    refactor(nvim): migrate autocmd to lua

--- .config/nvim/lua/conf/nvim_tree.lua
+++ .config/nvim/lua/conf/nvim_tree.lua
@@ -122,7 +122,17 @@ function M.config()
       hi link NvimTreeFolderIcon NonText
     ]]
 
-    vim.cmd [[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]]
+    vim.api.nvim_create_autocmd('BufEnter', {
+        nested = true,
+        callback = function()
+            if
+                #vim.api.nvim_list_wins() == 1
+                and vim.api.nvim_buf_get_name(0):match 'NvimTree_' ~= nil
+            then
+                vim.cmd 'quit'
+            end
+        end,
+    })
 end
 
 return M
