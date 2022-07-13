d2d2fe1cac4cdcd797d54f0e64c76bd1236d3b86
disrupted <hi@salomonpopp.me>
2022-07-13 22:13:15 +0200

    fix(nvim): restrict actionlint to GitHub workflows

--- .config/nvim/lua/conf/null-ls.lua
+++ .config/nvim/lua/conf/null-ls.lua
@@ -131,13 +131,12 @@ function M.config()
         null_ls.builtins.diagnostics.actionlint.with {
             -- based on https://github.com/jose-elias-alvarez/null-ls.nvim/pull/804
             runtime_condition = function()
-                local path = vim.api.nvim_buf_get_name(
-                    vim.api.nvim_get_current_buf()
-                )
+                local path =
+                    vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
                 return path:match 'github/workflows/' ~= nil
             end,
         },
-        null_ls.builtins.code_actions.refactoring,
+        -- null_ls.builtins.code_actions.refactoring,
         null_ls.builtins.code_actions.gitrebase,
     }
     return sources
