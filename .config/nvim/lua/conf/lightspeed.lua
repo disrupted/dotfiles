6f8e5ca6789281752b473970da3c41519fb22e8b
disrupted <hi@salomonpopp.me>
2022-10-10 20:08:33 +0200

    refactor(nvim): use new keymap API for lightspeed mappings

--- .config/nvim/lua/conf/lightspeed.lua
+++ .config/nvim/lua/conf/lightspeed.lua
@@ -2,29 +2,19 @@ local M = {}
 
 function M.setup()
     local default_keymaps = {
-        { 'n', 's', '<Plug>Lightspeed_s' },
-        { 'n', 'S', '<Plug>Lightspeed_S' },
-        { 'x', 's', '<Plug>Lightspeed_s' },
-        { 'x', 'S', '<Plug>Lightspeed_S' },
+        { { 'n', 'x' }, 's', '<Plug>Lightspeed_s' },
+        { { 'n', 'x' }, 'S', '<Plug>Lightspeed_S' },
         { 'o', 'z', '<Plug>Lightspeed_s' },
         { 'o', 'Z', '<Plug>Lightspeed_S' },
         { 'o', 'x', '<Plug>Lightspeed_x' },
         { 'o', 'X', '<Plug>Lightspeed_X' },
-        { 'n', 'f', '<Plug>Lightspeed_f' },
-        { 'n', 'F', '<Plug>Lightspeed_F' },
-        { 'x', 'f', '<Plug>Lightspeed_f' },
-        { 'x', 'F', '<Plug>Lightspeed_F' },
-        { 'o', 'f', '<Plug>Lightspeed_f' },
-        { 'o', 'F', '<Plug>Lightspeed_F' },
-        { 'n', 't', '<Plug>Lightspeed_t' },
-        { 'n', 'T', '<Plug>Lightspeed_T' },
-        { 'x', 't', '<Plug>Lightspeed_t' },
-        { 'x', 'T', '<Plug>Lightspeed_T' },
-        { 'o', 't', '<Plug>Lightspeed_t' },
-        { 'o', 'T', '<Plug>Lightspeed_T' },
+        { { 'n', 'x', 'o' }, 'f', '<Plug>Lightspeed_f' },
+        { { 'n', 'x', 'o' }, 'F', '<Plug>Lightspeed_F' },
+        { { 'n', 'x', 'o' }, 't', '<Plug>Lightspeed_t' },
+        { { 'n', 'x', 'o' }, 'T', '<Plug>Lightspeed_T' },
     }
     for _, m in ipairs(default_keymaps) do
-        vim.api.nvim_set_keymap(m[1], m[2], m[3], { silent = true })
+        vim.keymap.set(m[1], m[2], m[3], { silent = true })
     end
 end
 
@@ -32,7 +22,6 @@ function M.config()
     require('lightspeed').setup {
         ignore_case = true,
         exit_after_idle_msecs = { labeled = 4000, unlabeled = 3000 },
-        x_mode_prefix_key = '<c-x>', -- TODO
     }
 end
 
