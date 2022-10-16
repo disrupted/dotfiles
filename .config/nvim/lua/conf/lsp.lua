73bbfb86868147529b1539d94e4ccf9087891e57
disrupted <hi@salomonpopp.me>
2022-10-16 23:18:38 +0200

    refactor(nvim): use default LSP capabilities from cmp

--- .config/nvim/lua/conf/lsp.lua
+++ .config/nvim/lua/conf/lsp.lua
@@ -454,8 +454,7 @@ function M.config()
     -- client log level
     vim.lsp.set_log_level 'info'
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
     if pcall(require, 'vim.lsp.nvim-semantic-tokens') then
         require('nvim-semantic-tokens').setup {
