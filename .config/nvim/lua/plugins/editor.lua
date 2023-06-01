3b31f31ab8a2f16f88776d0bdc4c555509625efc
disrupted <hi@salomonpopp.me>
2023-06-01 11:32:48 +0200

    fix(nvim): quit when nvim-tree is last window

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -566,15 +566,33 @@ return {
             )
             vim.api.nvim_set_hl(0, 'NvimTreeFolderIcon', { link = 'NonText' })
 
-            vim.api.nvim_create_autocmd('BufEnter', {
-                nested = true,
+            -- trivial
+            -- vim.api.nvim_create_autocmd({ 'QuitPre' }, {
+            --     callback = function()
+            --         vim.cmd 'NvimTreeClose'
+            --     end,
+            -- })
+
+            vim.api.nvim_create_autocmd('QuitPre', {
                 callback = function()
-                    if
-                        #vim.api.nvim_list_wins() == 1
-                        and vim.api.nvim_buf_get_name(0):match 'NvimTree_'
-                            ~= nil
-                    then
-                        vim.cmd.quit()
+                    local invalid_wins = {}
+                    local wins = vim.api.nvim_list_wins()
+                    for _, w in ipairs(wins) do
+                        local bufname = vim.api.nvim_buf_get_name(
+                            vim.api.nvim_win_get_buf(w)
+                        )
+                        if
+                            bufname:match 'NvimTree_' ~= nil
+                            or vim.api.nvim_win_get_config(w).relative ~= '' -- floating window
+                        then
+                            table.insert(invalid_wins, w)
+                        end
+                    end
+                    if #invalid_wins == #wins - 1 then
+                        -- Should quit, so we close all invalid windows.
+                        for _, w in ipairs(invalid_wins) do
+                            vim.api.nvim_win_close(w, true)
+                        end
                     end
                 end,
             })
