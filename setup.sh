952458b61fb7ca951c5262a0715a4d73ff469059
disrupted <hi@salomonpopp.me>
2023-10-31 16:00:53 +0100

    docs: add Docker buildx

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 # Docker setup
 brew install colima
 brew install docker docker-compose
 mkdir -p ~/.docker/cli-plugins
-ln -sfn $(brew --prefix)/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
+ln -sfn "$(brew --prefix)"/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
+brew install docker-Buildx
+ln -sfn "$(brew --prefix)"/opt/docker-buildx/bin/docker-buildx ~/.docker/cli-plugins/docker-buildx
