b7a9607ec933c5fd1db2104a19880f48a2dc3a98
disrupted <hi@salomonpopp.me>
2024-01-09 10:06:30 +0100

    feat(macos): make windows draggable anywhere

--- .config/mac-defaults.sh
+++ .config/mac-defaults.sh
@@ -3,6 +3,8 @@
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
 
+defaults write -g NSWindowShouldDragOnGesture YES
+
 defaults write com.apple.screencapture name "screen"
 # shellcheck disable=SC2088
 defaults write com.apple.screencapture location "~/Pictures/Screenshots"
