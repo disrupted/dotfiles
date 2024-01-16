68cadc0e40b93887a0990ee7c19404174b1baf60
disrupted <hi@salomonpopp.me>
2024-01-16 18:05:37 +0100

    fix(nvim): remove keymap for GH actions
    interferes with Neogit (`<leader>g`)

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -211,13 +211,6 @@ return {
     {
         'topaxi/gh-actions.nvim',
         cmd = 'GhActions',
-        keys = {
-            {
-                '<leader>gh',
-                '<cmd>GhActions<cr>',
-                desc = 'Open Github Actions',
-            },
-        },
         build = 'make',
         dependencies = { 'nvim-lua/plenary.nvim', 'MunifTanjim/nui.nvim' },
         opts = {},
