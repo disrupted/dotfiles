6d3a69c8c19edeb9e718f3ce36b80d7bc35b5f5a
disrupted <hi@salomonpopp.me>
2024-02-02 01:23:02 +0100

    fix(nvim): delay in vmap depent `<`

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -75,7 +75,7 @@ return {
                 keymaps = {
                     init_selection = '<CR>', -- maps in normal mode to init the node/scope selection
                     node_incremental = '<CR>', -- increment to the upper named parent
-                    scope_incremental = '<nop>', -- increment to the upper scope (as defined in locals.scm)
+                    -- scope_incremental = '<nop>', -- increment to the upper scope (as defined in locals.scm)
                     node_decremental = '<BS>', -- decrement to the previous node
                 },
             },
