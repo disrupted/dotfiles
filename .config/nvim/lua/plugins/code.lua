6c8120977fc1bd4e45af2a49a34656a9ae5c269d
disrupted <hi@salomonpopp.me>
2024-07-23 22:50:35 +0200

    feat(nvim): configure ultimate-autopairs

--- .config/nvim/lua/plugins/code.lua
+++ .config/nvim/lua/plugins/code.lua
@@ -86,11 +86,40 @@ return {
         branch = 'v0.6',
         opts = {
             cmap = false,
+            extensions = {
+                cond = {
+                    -- disable in comments
+                    -- https://github.com/altermo/ultimate-autopair.nvim/blob/6fd0d6aa976a97dd6f1bed4d46be1b437613a52f/Q%26A.md?plain=1#L26
+                    cond = {
+                        function(fn)
+                            return not fn.in_node 'comment'
+                        end,
+                    },
+                },
+                -- get fly mode working on strings:
+                -- https://github.com/altermo/ultimate-autopair.nvim/issues/33
+                fly = {
+                    nofilter = true,
+                },
+            },
+            config_internal_pairs = {
+                { '"', '"', fly = true },
+                { '\'', '\'', fly = true },
+            },
             space2 = { enable = true },
             tabout = {
                 enable = true,
                 map = '<Tab>',
                 hopout = true,
+            },
+            { '\\(', '\\)' },
+            { '\\[', '\\]' },
+            { '\\{', '\\}' },
+            {
+                '<',
+                '>',
+                disable_start = true,
+                disable_end = true,
             },
         },
     },
