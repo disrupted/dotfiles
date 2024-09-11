a2d5c4f8f4b5a4a3f721d8e9cf826ed106515255
disrupted <hi@salomonpopp.me>
2024-09-11 14:56:38 +0200

    fix(nvim): disable cursorline for diffview

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -202,8 +202,17 @@ return {
     },
     {
         'sindrets/diffview.nvim',
-        cmd = { 'DiffviewFileOpen', 'DiffviewFileHistory' },
-        opts = { enhanced_diff_hl = true },
+        cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
+        opts = {
+            enhanced_diff_hl = true,
+            hooks = {
+                diff_buf_win_enter = function(bufnr, winid, ctx)
+                    -- Turn off cursor line for diffview windows because of bg conflict
+                    -- https://github.com/neovim/neovim/issues/9800
+                    vim.wo[winid].culopt = 'number'
+                end,
+            },
+        },
     },
     {
         'pwntester/octo.nvim',
