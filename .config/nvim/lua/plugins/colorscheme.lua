05c9ac016818c47ada4a8956ef0467f9aaf55763
disrupted <hi@salomonpopp.me>
2024-11-19 16:34:44 +0100

    feat(nvim): add mapping to toggle background between light and dark

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,20 @@ return {
         priority = 9999,
         config = function()
             require('one').colorscheme()
+
+            vim.keymap.set(
+                'n',
+                '<M-t>',
+                function()
+                    vim.o.background = (vim.o.background == 'light') and 'dark'
+                        or 'light'
+                end,
+                {
+                    noremap = true,
+                    silent = true,
+                    desc = 'Toggle background between light and dark mode',
+                }
+            )
         end,
     },
 }
