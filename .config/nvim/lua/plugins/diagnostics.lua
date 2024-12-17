ea2bd469da3ff6745d9dffc664641718cafffe11
disrupted <hi@salomonpopp.me>
2024-12-17 19:52:04 +0100

    refactor(nvim): disable lsp-lines plugin for Lazy

---
+++ .config/nvim/lua/plugins/diagnostics.lua
@@ -0,0 +1,50 @@
+return {
+    {
+        'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+        event = 'DiagnosticChanged',
+        init = function()
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = 'lazy',
+                desc = 'disable lsp_lines diagnostics for Lazy',
+                callback = function()
+                    local ns = vim.api.nvim_create_namespace 'lazy'
+                    vim.diagnostic.config({ virtual_lines = false }, ns)
+                end,
+            })
+        end,
+        config = function()
+            vim.diagnostic.config {
+                virtual_lines = { only_current_line = true },
+            }
+        end,
+    },
+    {
+        'rachartier/tiny-inline-diagnostic.nvim',
+        enabled = false,
+        event = 'DiagnosticChanged',
+        opts = {
+            signs = {
+                left = '',
+                right = '',
+                diag = '●',
+                arrow = '    ',
+                up_arrow = '    ',
+                vertical = ' │',
+                vertical_end = ' └',
+            },
+            hi = {
+                error = 'DiagnosticError',
+                warn = 'DiagnosticWarn',
+                info = 'DiagnosticInfo',
+                hint = 'DiagnosticHint',
+                arrow = 'NonText',
+                -- background = 'CursorLine',
+                -- mixing_color = 'None',
+                mixing_color = '#e7e7e7',
+            },
+            blend = {
+                factor = 0.27,
+            },
+        },
+    },
+}
