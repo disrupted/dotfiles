521c5d9db799a874c439094e4e20c035b6bc5c9c
disrupted <hi@salomonpopp.me>
2024-12-18 19:58:37 +0100

    feat(nvim): mark Home Assistant plugin as todo

--- .config/nvim/lua/plugins/lsp/custom/init.lua
+++ .config/nvim/lua/plugins/lsp/custom/init.lua
@@ -7,4 +7,8 @@ require('mason-lspconfig.mappings.server').lspconfig_to_package['pylance'] =
 
 require('lspconfig').pylance.setup {} -- HACK: mason-lspconfig doesn't detect it
 
+-- TODO
+-- from https://github.com/qosmio/nvim-config/blob/cab6eba656f602cd45cc335707c2bfc44bb39a38/registry/homeassistant.lua
+-- registry['homeassistant'] = 'homeassistant.mason'
+
 return registry
