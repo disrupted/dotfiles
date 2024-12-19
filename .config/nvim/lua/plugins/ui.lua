4a438fc326389d36fca6b843abe414d8f9b64b9f
disrupted <hi@salomonpopp.me>
2024-12-19 10:15:51 +0100

    refactor(nvim): configure dropbar for LSP document symbols

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -637,6 +637,7 @@ return {
                 desc = 'Pick symbols in winbar',
             },
         },
+        ---@type dropbar_configs_t
         opts = {
             bar = {
                 ---@type boolean|fun(buf: integer, win: integer, info: table?): boolean
@@ -650,10 +651,40 @@ return {
                         )
                         and vim.wo[win].winbar == ''
                         and (
-                            (pcall(vim.treesitter.get_parser, buf))
-                                and true
+                            (pcall(vim.treesitter.get_parser, buf)) and true
                             or false
                         )
+                end,
+                ---@type dropbar_source_t[]|fun(buf: integer, win: integer): dropbar_source_t[]
+                sources = function(buf, _)
+                    local sources = require 'dropbar.sources'
+                    if vim.bo[buf].ft == 'markdown' then
+                        return {
+                            sources.path,
+                            sources.markdown,
+                        }
+                    end
+                    if vim.bo[buf].buftype == 'terminal' then
+                        return {
+                            sources.terminal,
+                        }
+                    end
+
+                    local clients = vim.lsp.get_clients {
+                        bufnr = buf,
+                        method = require('vim.lsp.protocol').Methods.textDocument_documentSymbol,
+                    }
+                    if not vim.tbl_isempty(clients) then
+                        return {
+                            sources.path,
+                            sources.lsp,
+                        }
+                    else
+                        return {
+                            sources.path,
+                            sources.treesitter,
+                        }
+                    end
                 end,
             },
             sources = {
@@ -713,6 +744,19 @@ return {
                         'identifier',
                         'object',
                         'statement',
+                    },
+                },
+                lsp = {
+                    valid_symbols = {
+                        'File',
+                        'Module',
+                        'Namespace',
+                        'Package',
+                        'Class',
+                        'Method',
+                        'Constructor',
+                        'Interface',
+                        'Function',
                     },
                 },
             },
@@ -793,6 +837,35 @@ return {
                 },
             },
         },
+        config = function(_, opts)
+            require('dropbar').setup(opts)
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+                desc = 'Register LSP source and disable Treesitter source when an LS that supports documentSymbol attaches.',
+                group = vim.api.nvim_create_augroup(
+                    'DropBarLspAttachRefreshSources',
+                    { clear = true }
+                ),
+                callback = function(args)
+                    local bufnr = args.buf
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if
+                        client
+                        and client.supports_method 'textDocument/documentSymbol'
+                    then
+                        local sources = require 'dropbar.sources'
+                        local utils = require 'dropbar.utils'
+                        for win in pairs(_G.dropbar.bars[bufnr]) do
+                            _G.dropbar.bars[bufnr][win].sources = {
+                                sources.path,
+                                sources.lsp,
+                            }
+                            utils.bar.exec('update', { win = win })
+                        end
+                    end
+                end,
+            })
+        end,
     },
     { 'kyazdani42/nvim-web-devicons', lazy = true },
     {
