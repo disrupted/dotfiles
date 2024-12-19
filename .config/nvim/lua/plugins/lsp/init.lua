d789be5a073821b2e2a274093a773bd4827c4368
disrupted <hi@salomonpopp.me>
2024-12-19 22:44:54 +0100

    fix(nvim): configure lazydev for Snacks global

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -282,6 +282,7 @@ return {
                     library = {
                         -- Load luvit types when the `vim.uv` word is found
                         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+                        { path = 'snacks.nvim', words = { 'Snacks' } },
                     },
                 },
             },
