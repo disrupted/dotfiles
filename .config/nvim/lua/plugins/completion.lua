32a0efed790d255cd7d499c0be17ad22ec5c1a55
disrupted <hi@salomonpopp.me>
2025-01-09 21:11:20 +0100

    refactor(nvim)!: update blink.cmp

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -38,26 +38,13 @@ return {
                 --     end,
                 -- },
             },
-            snippets = {
-                expand = function(snippet)
-                    require('luasnip').lsp_expand(snippet)
-                end,
-                active = function(filter)
-                    if filter and filter.direction then
-                        return require('luasnip').jumpable(filter.direction)
-                    end
-                    return require('luasnip').in_snippet()
-                end,
-                jump = function(direction)
-                    require('luasnip').jump(direction)
-                end,
-            },
+            snippets = { preset = 'luasnip' },
             sources = {
                 default = {
                     'lazydev',
                     'lsp',
                     'path',
-                    'luasnip',
+                    'snippets',
                     'buffer',
                     'git',
                 },
