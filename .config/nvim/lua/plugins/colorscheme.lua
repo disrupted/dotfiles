1dc28ff6424541400f1b7ea97ffbf5ebaf63041b
disrupted <hi@salomonpopp.me>
2025-02-25 22:49:34 +0100

    refactor(nvim): standardize setup & keymap for colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,18 +5,17 @@ return {
         'disrupted/one.nvim', -- personal tweaked colorscheme
         lazy = false,
         priority = 1000,
-        config = function()
-            require('one').colorscheme()
-
-            vim.keymap.set('n', '<M-t>', function()
-                vim.o.background = (vim.o.background == 'light') and 'dark'
-                    or 'light'
-            end, {
-                noremap = true,
-                silent = true,
+        keys = {
+            {
+                '<M-t>',
+                function()
+                    vim.o.background = (vim.o.background == 'light') and 'dark'
+                        or 'light'
+                end,
                 desc = 'Toggle background between light and dark mode',
-            })
-        end,
+            },
+        },
+        config = true,
     },
     {
         'catppuccin/nvim',
