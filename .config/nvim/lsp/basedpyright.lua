f2cb99969b1e3b87c240a91eb3f62d8d57e8e544
disrupted <hi@salomonpopp.me>
2025-03-12 23:50:15 +0100

    feat(nvim): add custom highlighter for Python parameter reference using LSP semantic tokens + TreeSitter captures
    example:
    
    f = lambda normal, list, set=1: normal + list + set
    
    TreeSitter colors the references of `list` and `set` as Python builtins here as it
    doesn't have knowledge about them being function parameters.
    
    LSP on the other hand provides semantic tokens categorizing them as
    parameters but there is only a modifier for definition, not references.
    
    We can use the combined knowledge to highlight those edge cases
    correctly.

--- .config/nvim/lsp/basedpyright.lua
+++ .config/nvim/lsp/basedpyright.lua
@@ -1,18 +1,53 @@
 ---@type vim.lsp.Config
 return {
     cmd = { 'basedpyright-langserver', '--stdio' },
+    -- cmd = { 'npm', '-C', '~/dev/basedpyright', 'run', 'run:langserver' },
     filetypes = { 'python' },
     root_markers = {
         'pyproject.toml',
         'setup.py',
         '.git',
     },
+    on_attach = function(client, bufnr)
+        vim.api.nvim_create_autocmd('LspTokenUpdate', {
+            buffer = bufnr,
+            callback = function(args)
+                if args.data.client_id ~= client.id then
+                    return
+                end
+                ---@type STTokenRange
+                local token = args.data.token
+                if
+                    token.type == 'parameter' and not token.modifiers.definition
+                then
+                    local captures = vim.treesitter.get_captures_at_pos(
+                        bufnr,
+                        token.line,
+                        token.start_col
+                    )
+                    if
+                        vim.iter(captures):any(function(capture)
+                            return capture.lang == 'python'
+                                and capture.capture == 'variable.parameter'
+                        end)
+                    then
+                        -- inside function call, abort override
+                        return
+                    end
+                    vim.lsp.semantic_tokens.highlight_token(
+                        token,
+                        args.buf,
+                        args.data.client_id,
+                        '@variable.parameter.reference'
+                    )
+                end
+            end,
+        })
+    end,
     settings = {
         basedpyright = {
             analysis = {
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = false,
-                diagnosticMode = 'workspace',
+                diagnosticMode = 'workspace', -- default: openFilesOnly
             },
         },
     },
