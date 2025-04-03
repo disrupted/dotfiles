f87a57f6a966babc45dbbe9e1d176507f7efaab8
disrupted <hi@salomonpopp.me>
2025-04-03 14:49:43 +0200

    fix(nvim): do not ignore whitespace changes for Python & YAML diff
    indentation matters in these

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -1,1 +1,2 @@
 vim.opt.formatoptions:remove 'o' -- disable comment leader when opening new line
+vim.opt.diffopt:remove 'iwhite'
--- .config/nvim/after/ftplugin/yaml.lua
+++ .config/nvim/after/ftplugin/yaml.lua
@@ -1,3 +1,4 @@
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
+vim.opt.diffopt:remove 'iwhite'
