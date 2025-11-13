d446f39c4d7708a685ef84ebd88fb10d9ae5da01
disrupted <hi@salomonpopp.me>
2025-11-13 21:50:12 +0100

    refactor(nvim): improve sourcekit-lsp config

--- .config/nvim/lsp/sourcekit.lua
+++ .config/nvim/lsp/sourcekit.lua
@@ -1,41 +1,18 @@
+local root_markers = {
+    'buildServer.json',
+    '*.xcodeproj',
+    '*.xcworkspace',
+    '.git',
+}
+
 ---@type vim.lsp.Config
 return {
     cmd = { 'sourcekit-lsp' },
-    filetypes = { 'swift', 'objc', 'objcpp', 'c', 'cpp' },
-    root_markers = {
-        '*.xcodeproj',
-        '*.xcworkspace',
-        -- '.git',
-    },
-    -- root_dir = function(bufnr, on_dir)
-    --     local filename = vim.api.nvim_buf_get_name(bufnr)
-    --     -- on_dir(
-    --     --     util.root_pattern 'buildServer.json'(filename)
-    --     --         or util.root_pattern('*.xcodeproj', '*.xcworkspace')(filename)
-    --     --         -- better to keep it at the end, because some modularized apps contain multiple Package.swift files
-    --     --         or util.root_pattern('compile_commands.json', 'Package.swift')(
-    --     --             filename
-    --     --         )
-    --     --         or vim.fs.dirname(
-    --     --             vim.fs.find('.git', { path = filename, upward = true })[1]
-    --     --         )
-    --     -- )
-    -- end,
-    get_language_id = function(_, ftype)
-        local t = { objc = 'objective-c', objcpp = 'objective-cpp' }
-        return t[ftype] or ftype
+    filetypes = { 'swift' },
+    root_markers = root_markers,
+    root_dir = function(bufnr, on_dir)
+        local filename = vim.api.nvim_buf_get_name(bufnr)
+        local root = vim.fs.root(filename, root_markers)
+        on_dir(root)
     end,
-    capabilities = {
-        workspace = {
-            didChangeWatchedFiles = {
-                dynamicRegistration = true,
-            },
-        },
-        textDocument = {
-            diagnostic = {
-                dynamicRegistration = true,
-                relatedDocumentSupport = true,
-            },
-        },
-    },
 }
