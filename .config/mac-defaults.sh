cba900fd5b34a00c582b28a937760c9fa4e34dc8
disrupted <hi@salomonpopp.me>
2025-11-26 19:20:45 +0100

    chore(macos): disable universal clipboard

--- .config/mac-defaults.sh
+++ .config/mac-defaults.sh
@@ -9,3 +9,7 @@ defaults write com.apple.screencapture name "screen"
 # shellcheck disable=SC2088
 defaults write com.apple.screencapture location "~/Pictures/Screenshots"
 killall SystemUIServer
+
+# disable universal clipboard between iCloud devices while keeping Handoff turned on
+# requires full disk access for terminal app
+defaults write ~/Library/Preferences/com.apple.coreservices.useractivityd.plist ClipboardSharingEnabled 0
