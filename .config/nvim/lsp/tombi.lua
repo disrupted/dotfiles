85ba45e579393e39dc177c4651d792e3dd0d3e5a
disrupted <hi@salomonpopp.me>
2026-02-14 11:42:32 +0100

    refactor(nvim|lsp): migrate from taplo to tombi

--- .config/nvim/lsp/tombi.lua
+++ .config/nvim/lsp/tombi.lua
@@ -1,15 +1,8 @@
 ---@type vim.lsp.Config
 return {
-    cmd = { 'taplo', 'lsp', 'stdio' },
+    cmd = { 'tombi', 'lsp' },
     filetypes = { 'toml' },
     workspace_required = false,
-    settings = {
-        evenBetterToml = {
-            taplo = {
-                configFile = { enabled = true },
-            },
-        },
-    },
     root_dir = function(bufnr, cb)
         local fname = vim.api.nvim_buf_get_name(bufnr)
         cb(vim.fs.dirname(fname))
